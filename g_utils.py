c5045c1a543f5bd966f41553d796561ac54a08df
Gavin <gavin.feeney01@gmail.com>
2021-02-21 15:05:59 +0000

    Add basic functions ref API interaction (#4) Add basic functions ref settings file (#3)

--- g_utils.py
+++ g_utils.py
@@ -4,6 +4,8 @@ import subprocess
 import webbrowser
 import sys
 from datetime import datetime
+from nomics import Nomics
+import nomics
 
 
 class G_Utils:
@@ -46,8 +48,81 @@ class G_Utils:
         delta = now-date
         return delta.days
 
+    def getAPIKey():
+        filename = "settings.cfg"
+        # check if file exists otherwise create new file
+        if os.path.isfile(filename):
+            # then read for the key
+            with open(filename) as f:
+                line = f.readline()
+                # need to check if the file is valid first
+                if not "API Key = " in line:
+                    return False
+
+                else:
+                    # otherwise return the key
+                    return line.replace("API Key = ", "")
+
+        else:
+            # prompt the user for their api key
+            # for now this will be command line, update to having a window later
+            key = input("Enter your API Key: ")
+            # write they key to the file
+            with open(filename, "w") as file:
+                file.write("API Key = " + key)
+            # and finally return the key
+            return key
+
+    def getDefaultExchange():
+        # This will grab the default exchange from the settings file
+        pass
+
+    def setDefaultExchange():
+        # This will set the default exchange in the settings file
+        pass
+
+    def getRates(key):
+        if not key:
+            # if it isn't valid print an error message
+            print("Invalid settings file")
+        else:
+            nomics = Nomics(key)
+            return nomics.ExchangeRates.get_rates()
+
+        # then do some sorting of the output
+
+    def getMarkets(key, exch):
+        if not key:
+            # if it isn't valid print an error message
+            print("Invalid settings file")
+        else:
+            nomics = Nomics(key)
+            markets = nomics.Markets.get_markets(exchange=exch)
+            print(markets)
+
+        # then do some sorting out the output
+
+    def checkSettingsFileExists():
+        filename = "settings.cfg"
+        # check if file exists
+        if os.path.isfile(filename):
+            # Need to check if the file is valid first
+            # If valid then return setup complete
+            return "Setup Complete"
+            # Otherwise return invalid settings file
+        else:
+            return "Setup Incomplete"
+
+    def getNetWorth():
+        # This will get the approximate net worth of the user
+        # Against the latest exchange rates
+        # Placeholder for now
+        return 1000
+
 
 if __name__ == "__main__":
     # playground for testing
     # G_Utils.days_until_christmas()
+    # print(G_Utils.getMarkets(G_Utils.getAPIKey(), "binance"))
+    print(G_Utils.getRates(G_Utils.getAPIKey()))
     pass
