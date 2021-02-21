8ebeaa5ce9876ef1281c665e2e0f2ec98bd9c919
Gavin <gavin.feeney01@gmail.com>
2021-02-21 20:43:05 +0000

    Add placeholder plot to analysis_window (#1)

--- analysis_window.py
+++ analysis_window.py
@@ -1,5 +1,12 @@
 import tkinter as tk
 import tkinter.font as tkFont
+import matplotlib
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+
+# Small bit of setup, need to use TkAgg instead of default
+matplotlib.use("TkAgg")
 
 
 class AnalysisWindow:
@@ -7,14 +14,19 @@ class AnalysisWindow:
         self.mainwindow = mainwindow
         self.root = root
         self.root.geometry("400x400+400+400")
-        self.frame = tk.Frame(self.root)
-        self.quitButton = tk.Button(
-            self.frame, text='Quit', width=25, command=self.close_windows)
-        self.label = tk.Label(root, text=f"this is the analysis window")
+        self.label = tk.Label(root, text=f"This is the analysis window")
         self.label.pack()
-        self.quitButton.pack()
-        self.frame.pack()
 
-    def close_windows(self):
-        self.mainwindow.btnAnalysis["state"] = "normal"
-        self.root.destroy()
+        self.fig = Figure(figsize=(7, 7), dpi=115)
+        self.fig.add_subplot(1, 1, 1).plot(
+            [1, 2, 3, 4, 5], [100, 104, 121, 108, 132])
+
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
+        self.canvas.draw()
+
+        self.toolbar = NavigationToolbar2Tk(
+            self.canvas, self.root, pack_toolbar=False)
+        self.toolbar.update()
+
+        self.toolbar.pack(side=tk.BOTTOM, fill=tk.X)
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
