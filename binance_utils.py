1f76379486c4c73e1babfa88bbbbcbfbc0dba049
Gavin <gavin.feeney01@gmail.com>
2021-02-25 10:08:54 +0000

    Pivot towards different API's than before for #4

---
+++ binance_utils.py
@@ -0,0 +1,23 @@
+import os
+import time
+import subprocess
+import webbrowser
+from datetime import datetime
+import json
+import urllib.request
+
+# This is the class for calling to the bitfinex public api
+
+
+class BinanceUtils:
+    def get_coin_list():
+        pass
+
+    def call_api():
+        pass
+
+    def get_coin_candle():
+        pass
+
+    def get_net_worth():
+        pass
