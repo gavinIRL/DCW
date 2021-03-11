0e40f3821f141f8c798a2c95621cfc8f5c5fda9a
Gavin <gavin.feeney01@gmail.com>
2021-03-11 20:57:54 +0000

    Added animation of chart

--- chart_window.py
+++ chart_window.py
@@ -6,6 +6,10 @@ import matplotlib
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
+import random
+from matplotlib import pyplot
+from datetime import datetime
+from matplotlib.animation import FuncAnimation
 
 # Small bit of setup, need to use TkAgg instead of default
 matplotlib.use("TkAgg")
@@ -20,6 +24,8 @@ class ChartWindow:
         font_heading = tkFont.Font(family='Times', size=10, weight="bold")
         font_label = tkFont.Font(family='Times', size=10)
         font_title = tkFont.Font(family='Times', size=12, weight="bold")
+        self.current_data = [100, 104, 121, 108, 132]
+        self.current_data_time = [1, 2, 3, 4, 5]
 
         width = 400
         height = 401
@@ -41,7 +47,11 @@ class ChartWindow:
 
         self.combo_time = ttk.Combobox(
             root, textvariable=self.currency_shown)
-        self.combo_time.place(x=1, y=vert_position, width=149, height=25)
+        self.combo_time.place(x=1, y=vert_position, width=74, height=25)
+
+        self.combo_indicator = ttk.Combobox(
+            root, textvariable=self.currency_shown)
+        self.combo_indicator.place(x=75, y=vert_position, width=74, height=25)
 
         self.lbl_title = tk.Label(self.root)
         self.lbl_title["font"] = font_title
@@ -61,26 +71,46 @@ class ChartWindow:
             "text"] = "RSI(6)=12.2  |  RSI(14)=15.3  |  MA(50)=52100.12  |  HV(10) = 40"
         self.lbl_net_worth.place(x=0, y=vert_position, width=width, height=20)
 
-        self.fig = Figure(figsize=(7, 7), dpi=115)
-        self.fig.add_subplot(1, 1, 1).plot(
-            [1, 2, 3, 4, 5], [100, 104, 121, 108, 132])
+        # self.fig = Figure(figsize=(7, 7), dpi=115)
+        # self.ax = self.fig.add_subplot(1, 1, 1)
+        # self.ax.plot([1, 2, 3, 4, 5], [100, 104, 121, 108, 132])
 
-        self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
+        # self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
+        # self.canvas.draw()
+
+        # self.toolbar = NavigationToolbar2Tk(
+        #     self.canvas, self.root, pack_toolbar=False,)
+        # self.toolbar.update()
+
+        # vert_position += 25
+        # self.canvas.get_tk_widget().place(x=0, y=vert_position, width=width, height=300)
+        # vert_position += 305
+        # self.toolbar.place(relx=0.58, y=vert_position, width=300,
+        #                    height=30, anchor=CENTER)
+
+        self.xdata, self.ydata = [], []
+        self.figure = pyplot.figure()
+        self.line, = pyplot.plot_date(self.xdata, self.ydata, "-")
+        # self.figure.place(relx=0.58, y=vert_position,
+        #                   width=300, height=30, anchor=CENTER)
+
+        self.canvas = FigureCanvasTkAgg(self.figure, master=self.root)
         self.canvas.draw()
 
-        self.toolbar = NavigationToolbar2Tk(
-            self.canvas, self.root, pack_toolbar=False,)
-        self.toolbar.update()
-
-        vert_position += 25
-        self.canvas.get_tk_widget().place(x=0, y=vert_position, width=width, height=300)
-        vert_position += 305
-        self.toolbar.place(relx=0.58, y=vert_position, width=300,
-                           height=30, anchor=CENTER)
+    def update_figure(self, frame):
+        self.xdata.append(datetime.now())
+        self.ydata.append(random.randint(80, 120))
+        self.line.set_data(self.xdata, self.ydata)
+        self.figure.gca().relim()
+        self.figure.gca().autoscale_view()
+        pyplot.show()
+        return self.line,
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.attributes('-toolwindow', True)
     cw = ChartWindow(None, root)
+    animation = FuncAnimation(cw.figure, cw.update_figure, interval=200)
+
     root.mainloop()
