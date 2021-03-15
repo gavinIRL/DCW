92c71f35801b40bb78c5dcf774803d89fd926c7e
Gavin <gavin.feeney01@gmail.com>
2021-03-15 16:36:26 +0000

    Initial commit towards #22

--- logger.py
+++ logger.py
@@ -8,6 +8,9 @@
 # 5) Potentially update the format to include more information later
 from dcw_utils import DCWUtils
 import time
+from requests.exceptions import HTTPError
+import json
+import requests
 
 
 class StandaloneLogger():
@@ -19,6 +22,55 @@ class StandaloneLogger():
         self.fresh_prices = []
         for curr in self.market_currency_list:
             self.fresh_prices.append(1.2)
+
+    def get_tick_logger(self, pair=False):
+        if isinstance(pair, str):
+            data = self.request_api_logger("ticker/price?symbol="+pair)
+            return str(data["price"]).strip("0")
+        elif isinstance(pair, list):
+            data = self.request_api_logger("ticker/price")
+            return_data = {}
+            for entry in data:
+                if entry["symbol"] in pair:
+                    stripped = str(entry["price"]).rstrip("0")
+                    return_data.update({entry["symbol"]: stripped})
+            return return_data
+        else:
+            data = self.request_api_logger("ticker/price")
+            return data
+
+    def request_api_logger(self, suffix):
+        url = "https://api.binance.com/api/v3/" + suffix
+        try:
+            response = requests.request("GET", url)
+            if response.status_code == 200:
+                return json.loads(response.text)
+            elif response.status_code == 404:
+                print("Incorrect input, got 404'd")
+                return False
+            elif response.status_code == 429 or response.status_code == 418:
+                wait_seconds = int(response.headers["Retry-After"])
+                print("Making too many requests, on the naughty step for " +
+                      str(wait_seconds)+" seconds")
+                time.sleep(wait_seconds)
+                return False
+            else:
+                print(response.status_code)
+                return False
+        # This is to catch in case the above raises errors rather than returning codes
+        except HTTPError as err:
+            if err.code == 404:
+                print("Incorrect input, got 404'd")
+                return False
+            elif err.code == 429 or err.code == 418:
+                wait_seconds = int(response.headers["Retry-After"])
+                print("Making too many requests, on the naughty step for " +
+                      str(wait_seconds)+" seconds")
+                time.sleep(wait_seconds)
+                return False
+            else:
+                print(err.code)
+                return False
 
 
 if __name__ == "main":
