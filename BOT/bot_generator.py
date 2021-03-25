1264b1a51c0a52e3d309d71272adcb225901c7ad
Gavin <gavin.feeney01@gmail.com>
2021-03-25 20:25:11 +0000

    Add description and skeleton for #41

--- BOT/bot_generator.py
+++ BOT/bot_generator.py
@@ -1,4 +1,15 @@
-
+# This class will create a schema for how a ping-pong bot functions
+# The idea will be to have an object that can be iteratively created
+# And can have as many or few indicators to utilise
+# It will generally work by being fed a timepoint in the evaluator class
+# And then make a decision on whether to buy (if funds exist to buy)
+# Or alternatively sell if funds exist to sell
 class BotGenerator():
     def __init__(self) -> None:
+        pass
+
+    def check_buy(self):
+        pass
+
+    def check_sell(self):
         pass
