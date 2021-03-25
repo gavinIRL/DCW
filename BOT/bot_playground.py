da1cd9a18673a2df1341de8a68e65cf5007b77c5
Gavin <gavin.feeney01@gmail.com>
2021-03-25 20:28:38 +0000

    Add description and skeleton for #40

--- BOT/bot_playground.py
+++ BOT/bot_playground.py
@@ -1,4 +1,20 @@
-
+# This class will iteratively create and evaluate bots
+# Using the generator and evaluator classes
+# This will be the primary class
+# The results will be output to csv
+# And also there will be a visualisation of the data available
 class BotPlayground():
     def __init__(self) -> None:
+        pass
+
+    def create_bot(self):
+        pass
+
+    def output_results(self):
+        pass
+
+    def display_results(self):
+        pass
+
+    def start_iterating(self):
         pass
