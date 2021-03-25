c8e910e8ca2dbda7d259398e43995a2bd0c950e7
Gavin <gavin.feeney01@gmail.com>
2021-03-25 20:21:06 +0000

    Add description and skeleton for #42

--- BOT/bot_evaluator.py
+++ BOT/bot_evaluator.py
@@ -1,4 +1,18 @@
-
+# This class will provide the functions for evaluating a bot object
+# It will be called by the playground object
+# It will randomly select existing data
+# And then apply the given bot object to calculate when a buy/sell occurs
+# Calculate the metrics associated with the given bot behaviour
+# And then finally pass performance metrics back to the playground object
 class BotEvaluator():
     def __init__(self) -> None:
+        pass
+
+    def grab_historical_data(self):
+        pass
+
+    def calculate_buy_sell(self):
+        pass
+
+    def calculate_performance(self):
         pass
