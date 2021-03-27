37e91ba61ca586aa223935470a072172151440c6
Gavin <gavin.feeney01@gmail.com>
2021-03-27 15:23:58 +0000

    Add historical data loading for #42

--- BOT/bot_evaluator.py
+++ BOT/bot_evaluator.py
@@ -4,15 +4,35 @@
 # And then apply the given bot object to calculate when a buy/sell occurs
 # Calculate the metrics associated with the given bot behaviour
 # And then finally pass performance metrics back to the playground object
+from os import walk
+import csv
+import random
+
+
 class BotEvaluator():
     def __init__(self) -> None:
-        pass
+        self.data_for_eval = []
 
     def grab_historical_data(self):
-        pass
+        list_filenames = []
+        mypath = "C:\\DCWLog\\Test\\BTCUSDT\\"
+        for (_, _, filenames) in walk(mypath):
+            list_filenames.extend(filenames)
+            break
+        # print(list_filenames)
+        # Randomly choose one of the files for assessment (skeleton only)
+        with open(mypath+random.choice(list_filenames)) as csv_file:
+            reader = csv.reader(csv_file)
+            self.data_for_eval = list(reader)
+        print(self.data_for_eval)
 
     def calculate_buy_sell(self):
         pass
 
     def calculate_performance(self):
         pass
+
+
+if __name__ == "__main__":
+    be = BotEvaluator()
+    be.grab_historical_data()
