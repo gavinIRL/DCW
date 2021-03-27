2dfc0a013b780bd8a62f460f3d35e2b39a909c4e
Gavin <gavin.feeney01@gmail.com>
2021-03-27 07:19:06 +0000

    Rename gui folder to watcher

---
+++ watcher/dcw_utils.py
@@ -0,0 +1,326 @@
+import os
+import datetime
+import json
+import requests
+import numpy as np
+import pandas as pd
+import numba as nb
+import time
+from requests.exceptions import HTTPError
+import threading
+import queue
+import talib as ta
+
+# This is the class that contains the workhorse functions
+# I admit it is a really confused class, over 50% static methods
+
+
+class DCWUtils():
+
+    def __init__(self, exchange="Bitfinex", wallet=None):
+        self.exchange = exchange
+        self.wallet = wallet
+        if self.exchange == "Binance":
+            self.url_base = "https://api.binance.com/api/v3/"
+            # Actually 1000 weight/minute but better to use a slower pace
+            self.update_rate = 1000
+        elif self.exchange == "Bitfinex":
+            self.url_base = "https://api-pub.bitfinex.com/v2/"
+            self.update_rate = 1000
+        else:
+            print("Only Bitfinex and Binance supported so far")
+        # These are for csv logging
+        self.buffer_counter = 0
+        self.log_start_time = 0
+        self.buffer_data = []
+        self.buffer_times = []
+
+    def request_api(self, suffix):
+        url = self.url_base + suffix
+        try:
+            response = requests.request("GET", url)
+            if response.status_code == 200:
+                return json.loads(response.text)
+            elif response.status_code == 404:
+                print("Incorrect input, got 404'd")
+                return False
+            elif response.status_code == 429 or response.status_code == 418:
+                wait_seconds = int(response.headers["Retry-After"])
+                print("Making too many requests, on the naughty step for " +
+                      str(wait_seconds)+" seconds")
+                time.sleep(wait_seconds)
+                return False
+            else:
+                print(response.status_code)
+                return False
+        # This is to catch in case the above raises errors rather than returning codes
+        except HTTPError as err:
+            if err.code == 404:
+                print("Incorrect input, got 404'd")
+                return False
+            elif err.code == 429 or err.code == 418:
+                wait_seconds = int(response.headers["Retry-After"])
+                print("Making too many requests, on the naughty step for " +
+                      str(wait_seconds)+" seconds")
+                time.sleep(wait_seconds)
+                return False
+            else:
+                print(err.code)
+                return False
+
+    @staticmethod
+    def convert_ms_to_datetime(ms: int):
+        base_datetime = datetime.datetime(1970, 1, 1)
+        delta = datetime.timedelta(0, 0, 0, ms*1000)
+        target_date = base_datetime + delta
+        return target_date
+
+    @staticmethod
+    def create_suffix_bitfinex(currency="BTC", base="USD", data_type="Candle", custom_time=False, custom_limit=False):
+        pass
+
+    @staticmethod
+    def create_suffix_binance(currency="BTC", base="USDT", data_type="Candle", custom_time=False, custom_limit=False):
+        pass
+
+    def get_coin_list(self, raw=False):
+        if self.exchange == "Binance":
+            data = self.create_suffix_binance()
+            # Need to only send back the pairs that contain USD
+        elif self.exchange == "Bitfinex":
+            return self.request_api()
+        else:
+            return False
+
+    def get_tick(self, pair=False):
+        if self.exchange == "Binance":
+            if isinstance(pair, str):
+                data = self.request_api("ticker/price?symbol="+pair)
+                return str(data["price"]).strip("0")
+            elif isinstance(pair, list):
+                data = self.request_api("ticker/price")
+                return_data = {}
+                for entry in data:
+                    if entry["symbol"] in pair:
+                        stripped = str(entry["price"]).rstrip("0")
+                        return_data.update({entry["symbol"]: stripped})
+                return return_data
+            else:
+                data = self.request_api("ticker/price")
+                return data
+
+    def get_candle(self, pair, interval, limit, raw=False):
+        if self.exchange == "Binance":
+            data = self.request_api(
+                "klines?symbol="+pair+"&interval="+interval+"&limit="+str(limit))
+            if raw:
+                return data
+            else:
+                return_data = []
+                for row in data:
+                    # Don't want all the entries
+                    timepoint = {}
+                    timepoint["Open Time"] = row[0]
+                    timepoint["Open"] = row[1]
+                    timepoint["High"] = row[2]
+                    timepoint["Low"] = row[3]
+                    timepoint["Close"] = row[4]
+                    timepoint["Volume"] = row[5]
+                    timepoint["Close Time"] = row[6]
+                    return_data.append(timepoint)
+                return return_data
+
+    def get_coin_candle(self, currency="BTC", base="USDT", interval="15m", limit=10, target=False, include_time=True, time_ms=False):
+        if self.exchange == "Binance":
+            data = self.request_api(
+                "klines?symbol="+currency+base+"&interval="+interval+"&limit="+str(limit))
+            # This will simply return the raw data
+            if not target:
+                return data
+            # Otherwise return the requested data
+            return_data = []
+            return_time_data = []
+            for timepoint in data:
+                if include_time:
+                    return_time_data.append(timepoint[0])
+                if target == "Open Time":
+                    # This gets sent back anyway if include_time is true
+                    continue
+                elif target == "Open":
+                    return_data.append(timepoint[1])
+                elif target == "High":
+                    return_data.append(timepoint[2])
+                elif target == "Low":
+                    return_data.append(timepoint[3])
+                elif target == "Close":
+                    return_data.append(timepoint[4])
+                elif target == "Volume":
+                    return_data.append(timepoint[5])
+                elif target == "Close Time":
+                    return_data.append(timepoint[6])
+                elif target == "Quote Asset Volume":
+                    return_data.append(timepoint[7])
+                elif target == "Number of Trades":
+                    return_data.append(timepoint[8])
+                else:
+                    # Incorrect target provided
+                    return False
+            if not include_time:
+                return return_data
+            else:
+                # Convert times to datetime objects if milliseconds is false
+                if not time_ms:
+                    for i, entry in enumerate(return_time_data):
+                        return_time_data[i] = self.convert_ms_to_datetime(
+                            entry)
+                return list(zip(return_time_data, return_data))
+        elif self.exchange == "Bitfinex":
+            return self.create_suffix_bitfinex()
+        else:
+            return False
+
+    def csv_logger(self, pairs: list, time_list=False, existing_data=False, buffer=10, path=False):
+        # This can accept multiple lines of data at once
+        start_time = None
+        new_data = []
+        if not existing_data:
+            # Grab the data first as it isn't provided
+            start_time = time.time()
+            new_data = []
+            pass
+        else:
+            start_time = time_list[0]
+            new_data = existing_data[0]
+        # Then check whether to use the start time value
+        clean_format_time = str(self.convert_ms_to_datetime(
+            start_time)).replace("-", "")
+        clean_format_time = clean_format_time.replace(" ", "-").split(".")[0]
+        clean_format_time = clean_format_time.replace(":", "")
+        if self.log_start_time == 0:
+            self.log_start_time = clean_format_time
+        # Then update the buffer data
+        self.buffer_times.append(clean_format_time)
+        self.buffer_data.append(new_data.copy())
+        # Then write to the files every time 10 ticks are saved up
+        if self.buffer_counter >= buffer:
+            self.buffer_counter = 0
+            for i, pair in enumerate(pairs):
+
+                filename = str(pair)+"-" + \
+                    str(self.log_start_time)+".csv"
+                if path:
+                    folder_path = str(path) + str(pair) + "/"
+                    if not os.path.exists(folder_path):
+                        os.mkdir(folder_path)
+                    filename = folder_path + str(filename)
+                else:
+                    folder_path = "D:/DCWLog/" + str(pair) + "/"
+                    if not os.path.exists(folder_path):
+                        os.mkdir(folder_path)
+                    filename = folder_path + filename
+                # Check if file already exists
+                if not os.path.isfile(filename):
+                    with open(filename, "w") as file:
+                        for j, line in enumerate(self.buffer_times):
+                            csv_data = self.buffer_data[j][i]
+                            file.write(str(line)+","+str(csv_data)+"\n")
+                # Otherwise append
+                else:
+                    with open(filename, "a") as file:
+                        for j, line in enumerate(self.buffer_times):
+                            csv_data = self.buffer_data[j][i]
+                            file.write(str(line)+","+str(csv_data)+"\n")
+            # Then reset the buffer
+            self.buffer_times = []
+            self.buffer_data = []
+        else:
+            self.buffer_counter += 1
+
+    def get_net_worth(self, currency_list):
+        pass
+
+    @ staticmethod
+    def get_wallet():
+        pass
+
+    @ staticmethod
+    def set_wallet(currency_list):
+        pass
+
+    def calculate_ma(sequence, time_period):
+        return ta.MA(np.array(sequence), time_period)
+
+    def calculate_ema(sequence, time_period):
+        return ta.EMA(np.array(sequence), time_period)
+
+    def calculate_mom(sequence, time_period):
+        return ta.MOM(np.array(sequence), time_period)
+
+    def calculate_trix(sequence, time_period):
+        return ta.TRIX(np.array(sequence), time_period)
+
+    def calculate_dema(sequence, time_period):
+        return ta.DEMA(np.array(sequence), time_period)
+
+    def calculate_tema(sequence, time_period):
+        return ta.TEMA(np.array(sequence), time_period)
+
+    def calculate_rsi_talib(sequence, n):
+        return ta.RSI(np.array(sequence), n)
+
+    @ nb.jit(fastmath=True, nopython=True)
+    def calculate_rsi(array, deltas, avg_gain, avg_loss, n):
+
+        # Use Wilder smoothing method
+        def up(x): return x if x > 0 else 0
+        def down(x): return -x if x < 0 else 0
+        i = n+1
+        for d in deltas[n+1:]:
+            avg_gain = ((avg_gain * (n-1)) + up(d)) / n
+            avg_loss = ((avg_loss * (n-1)) + down(d)) / n
+            if avg_loss != 0:
+                rs = avg_gain / avg_loss
+                array[i] = 100 - (100 / (1 + rs))
+            else:
+                array[i] = 100
+            i += 1
+
+        return array
+
+    def get_rsi(array, n=2):
+        deltas = np.append([0], np.diff(array))
+        avg_gain = np.sum(deltas[1:n+1].clip(min=0)) / n
+        avg_loss = -np.sum(deltas[1:n+1].clip(max=0)) / n
+        array = np.empty(deltas.shape[0])
+        array.fill(np.nan)
+        array = DCWUtils.calculate_rsi(array, deltas, avg_gain, avg_loss, n)
+        # Now remove the NaN's when returning
+        array = array[~np.isnan(array)]
+        return array
+
+    @ staticmethod
+    def checkSettingsFileExists():
+        # check if file exists
+        if os.path.isfile("settings.cfg"):
+            # Need to check if the file is valid first
+            # If valid then return setup complete
+            return True
+            # Otherwise return invalid settings file
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    # playground for testing
+    dcw = DCWUtils("Binance")
+    # print(dcw.request_api("candles/trade:1m:tBTCUSD/hist?limit=10", True))
+    # output = dcw.get_coin_candle(target="High")
+    # output = dcw.get_candle("BTCUSDT", "1m", 10)
+    input = [2, 1.5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+             1, 2, 3, 4, 5, 6, 4, 5, 6, 5, 6, 5, 6, 5, 4, 3, 2, 1, 0.5, 4, 9]
+    # print(input)
+    #output = DCWUtils.get_rsi(input, n=6)
+    # print(output)
+    #print(DCWUtils.calculate_rsi_talib(input, 5))
+    # print(ta.get_functions())
+    print(DCWUtils.calculate_dema(input, 5))
---
+++ watcher/main_window.py
@@ -0,0 +1,266 @@
+
+import tkinter as tk
+import tkinter.font as tkFont
+from dcw_utils import DCWUtils
+from chart_window import ChartWindow
+from wallet_window import WalletWindow
+from settings_window import SettingsWindow
+from alerts_window import AlertsWindow
+from market_window import MarketWindow
+import queue
+import time
+import threading
+
+
+class MainWindow():
+
+    def __init__(self, root):
+        self.root = root
+
+        # Testing variables
+        # self.test_button = tk.Button(
+        #    self.root, command=self.tb_click, text="Test")
+        #self.test_button.place(x=0, y=230, width=300, height=30)
+        self.threads = []
+
+        # set variables for handling windows
+        self.new_window_chart = None
+        self.new_window_wallet = None
+        self.new_window_settings = None
+        self.new_window_alerts = None
+        self.new_window_market = None
+        # this will hold the marketwindow itself
+        self.mw = None
+        # set variables that hold the settings
+        self.update_time = 1000
+        self.market_currency_list = [
+            "BTCUSDT", "ETHUSDT", "ADAUSDT", "BNBUSDT", "DOTUSDT", "XRPUSDT", "LTCUSDT", "XLMUSDT",
+            "BCHUSDT", "DOGEUSDT", "XEMUSDT", "ATOMUSDT", "XMRUSDT", "EOSUSDT", "TRXUSDT"]
+        # This is for csv logging
+        self.enable_logging = False
+        self.log_loop_tracker = -5
+        self.fresh_prices = []
+        for curr in self.market_currency_list:
+            self.fresh_prices.append(1.2)
+        # This is for holding the wallet details
+        self.current_wallet = []
+        # set fonts
+        font_button = tkFont.Font(family='Times', size=12)
+        font_label = tkFont.Font(family='Times', size=12)
+        font_status = tkFont.Font(family='Times', size=12, weight="bold")
+        # setting title
+        self.root.title("DCW Main Menu")
+        # setting window size
+        width = 300
+        height = 331
+        width_screen = self.root.winfo_screenwidth()
+        height_screen = self.root.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height,
+                                    (width_screen - width) / 2, (height_screen - height) / 2)
+        self.root.geometry(alignstr)
+        self.root.resizable(width=False, height=False)
+
+        # self.btn_chart = tk.Button(self.root)
+        # self.btn_chart["font"] = font_button
+        # self.btn_chart["justify"] = "center"
+        # self.btn_chart["text"] = "Open Chart Window"
+        # self.btn_chart.place(x=0, y=230, width=width, height=30)
+        # self.btn_chart["command"] = self.btn_chart_command
+
+        self.btn_settings = tk.Button(self.root)
+        self.btn_settings["font"] = font_button
+        self.btn_settings["justify"] = "center"
+        self.btn_settings["text"] = "Open Settings"
+        self.btn_settings.place(x=0, y=160, width=width, height=30)
+        self.btn_settings["command"] = self.btn_settings_command
+
+        self.lbl_bitcoin_price = tk.Label(self.root)
+        self.lbl_bitcoin_price["font"] = font_label
+        self.lbl_bitcoin_price["justify"] = "center"
+        # This gets updated later, but a placeholder for startup
+        self.lbl_bitcoin_price["text"] = "Bitcoin price: $190"
+        self.lbl_bitcoin_price.place(x=0, y=125, width=width, height=30)
+
+        self.lbl_bitcoin_hilo = tk.Label(self.root)
+        self.lbl_bitcoin_hilo["font"] = font_label
+        self.lbl_bitcoin_hilo["justify"] = "center"
+        # This gets updated later, but a placeholder for startup
+        self.lbl_bitcoin_hilo["text"] = "Bitcoin: 24hr high $200 | low $180"
+        self.lbl_bitcoin_hilo.place(x=0, y=95, width=width, height=30)
+
+        self.btn_wallet = tk.Button(self.root)
+        self.btn_wallet["font"] = font_button
+        self.btn_wallet["justify"] = "center"
+        self.btn_wallet["text"] = "Open Wallet"
+        self.btn_wallet.place(x=0, y=195, width=width, height=30)
+        self.btn_wallet["command"] = self.btn_wallet_command
+
+        self.btn_alerts = tk.Button(self.root)
+        self.btn_alerts["font"] = font_button
+        self.btn_alerts["justify"] = "center"
+        self.btn_alerts["text"] = "Open Alerts"
+        self.btn_alerts.place(x=0, y=300, width=width, height=30)
+        self.btn_alerts["command"] = self.btn_alerts_command
+
+        self.lbl_user_status = tk.Label(self.root)
+        self.lbl_user_status["font"] = font_status
+        self.lbl_user_status["justify"] = "center"
+        self.lbl_user_status["text"] = "User Setup Complete: " + \
+            str(DCWUtils.checkSettingsFileExists())
+        self.lbl_user_status.place(x=0, y=5, width=width, height=30)
+
+        self.lbl_net_worth = tk.Label(self.root)
+        self.lbl_net_worth["font"] = font_status
+        self.lbl_net_worth["justify"] = "center"
+        # This gets updated later, but a placeholder for startup
+        self.lbl_net_worth["text"] = "User Net Worth: $1000"
+        self.lbl_net_worth.place(x=0, y=35, width=width, height=30)
+
+        self.btn_market = tk.Button(self.root)
+        self.btn_market["font"] = font_button
+        self.btn_market["justify"] = "center"
+        self.btn_market["text"] = "Open Market Overview"
+        self.btn_market.place(x=0, y=265, width=width, height=30)
+        self.btn_market["command"] = self.btn_market_command
+
+        self.lbl_net_worth_incr = tk.Label(self.root)
+        self.lbl_net_worth_incr["font"] = font_label
+        self.lbl_net_worth_incr["justify"] = "center"
+        # This gets updated later, but a placeholder for startup
+        self.lbl_net_worth_incr["text"] = "User 24hr Net Worth Increase: 11%"
+        self.lbl_net_worth_incr.place(x=0, y=65, width=width, height=30)
+
+    def destroy_wallet(self):
+        self.new_window_wallet.destroy()
+        self.btn_wallet["state"] = "normal"
+
+    def destroy_settings(self):
+        self.new_window_settings.destroy()
+        self.btn_settings["state"] = "normal"
+
+    def destroy_alerts(self):
+        self.new_window_alerts.destroy()
+        self.btn_alerts["state"] = "normal"
+
+    def destroy_market(self):
+        self.new_window_market.destroy()
+        self.btn_market["state"] = "normal"
+        self.new_window_market = None
+        self.mw = None
+
+    def new_window(self, _class, **kwargs):
+        if _class is WalletWindow:
+            self.btn_wallet["state"] = "disabled"
+            self.new_window_wallet = tk.Toplevel(self.root)
+            self.new_window_wallet.protocol(
+                "WM_DELETE_WINDOW", self.destroy_wallet)
+            _class(self, self.new_window_wallet)
+        if _class is ChartWindow:
+            # self.btn_chart["state"] = "disabled"
+            self.new_window_chart = tk.Toplevel(self.root)
+            # self.new_window_chart.protocol(
+            #     "WM_DELETE_WINDOW", self.destroy_analysis)
+            _class(master=self.new_window_chart, **kwargs)
+        if _class is SettingsWindow:
+            self.btn_settings["state"] = "disabled"
+            self.new_window_settings = tk.Toplevel(self.root)
+            self.new_window_settings.protocol(
+                "WM_DELETE_WINDOW", self.destroy_settings)
+            _class(self, self.new_window_settings)
+        if _class is AlertsWindow:
+            self.btn_alerts["state"] = "disabled"
+            self.new_window_alerts = tk.Toplevel(self.root)
+            self.new_window_alerts.protocol(
+                "WM_DELETE_WINDOW", self.destroy_alerts)
+            _class(self, self.new_window_alerts)
+        if _class is MarketWindow:
+            self.btn_market["state"] = "disabled"
+            self.new_window_market = tk.Toplevel(self.root)
+            self.new_window_market.protocol(
+                "WM_DELETE_WINDOW", self.destroy_market)
+            self.mw = _class(self, self.new_window_market,
+                             currencies=self.market_currency_list)
+
+    def btn_settings_command(self):
+        self.new_window(SettingsWindow)
+
+    def btn_alerts_command(self):
+        self.new_window(AlertsWindow)
+
+    # def btn_chart_command(self):
+    #     self.new_window(ChartWindow)
+
+    def btn_wallet_command(self):
+        self.new_window(WalletWindow)
+
+    def btn_market_command(self):
+        self.new_window(MarketWindow)
+
+    def update_bitcoin_price(self, price=1337):
+        self.lbl_bitcoin_price["text"] = "Bitcoin price: $"+str(price)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.attributes('-toolwindow', True)
+    MainWindow = MainWindow(root)
+    dcw = DCWUtils(exchange="Binance")
+    # Having this as a function for readability only
+
+    def startup():
+        # Perform the startup functions such as grabbing settings
+        # The prices will be grabbed in update prices so no need to do anything here
+        # Only going to update the bitcoin hilo once
+        dcw = DCWUtils("Binance")
+        data_hilo = dcw.get_candle("BTCUSDT", "1d", 1)[0]
+        day_high = float(data_hilo["High"])
+        day_low = float(data_hilo["Low"])
+        hilo_text = "Bitcoin: 24hr high $" + \
+            str(day_high)+" | low $"+str(day_low)
+        MainWindow.lbl_bitcoin_hilo["text"] = hilo_text
+
+    def update_prices():
+        # Perform an update of the information
+        # These are all either placeholder or call placeholder functions
+        # If the market window isn't open then update at a slower rate of once per 15 seconds
+        ticker_time = 5000
+        if MainWindow.new_window_market != None:
+            ticker_time = MainWindow.update_time
+            # Then update the prices in the market window
+            current_time = time.time()
+            fresh_data = dcw.get_tick(MainWindow.market_currency_list)
+
+            # Update the relevant windows
+            MainWindow.mw.update_all_prices(fresh_data)
+            MainWindow.update_bitcoin_price(fresh_data["BTCUSDT"])
+
+            if MainWindow.enable_logging:
+                # Prepare data for logging in csv
+                for pair, price in fresh_data.items():
+                    index = MainWindow.market_currency_list.index(pair)
+                    MainWindow.fresh_prices[index] = price
+                # And then send the data for logging after the first few loops
+                # Will eventually make it so that it starts appending to a new file every hour
+                if MainWindow.log_loop_tracker > 0:
+                    dcw.csv_logger(pairs=MainWindow.market_currency_list, time_list=[
+                        current_time], existing_data=[MainWindow.fresh_prices])
+                # Will do something like grab a create a new file every day later
+                MainWindow.log_loop_tracker += 1
+
+        # Otherwise grab the data and update mainwindow only
+        else:
+            MainWindow.update_bitcoin_price(dcw.get_tick("BTCUSDT"))
+            # And clear any leftover buffer data from previous windows
+            MainWindow.log_loop_tracker = -5
+            dcw.buffer_counter = 0
+            dcw.buffer_data = []
+            dcw.buffer_times = []
+            dcw.log_start_time = 0
+
+        root.after(ticker_time, update_prices)
+
+    # Now perform the startup process and update as required
+    root.after(500, startup)
+    root.after(1000, update_prices)
+    root.wm_attributes('-topmost', True)
+    root.mainloop()
